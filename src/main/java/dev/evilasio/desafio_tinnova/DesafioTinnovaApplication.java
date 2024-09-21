55c437f948eaf1942c988d2dbe0c10a110b60b1e
Evilasio Neto <evilasio_neto12@hotmail.com>
2024-09-21 13:37:59 -0300

    desafio iniciado

---
+++ src/main/java/dev/evilasio/desafio_tinnova/DesafioTinnovaApplication.java
@@ -0,0 +1,13 @@
+package dev.evilasio.desafio_tinnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTinnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTinnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evilasio/desafio_tinnova/DesafioTinnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evilasio.desafio_tinnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTinnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
